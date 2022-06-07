83659a9d15bd09f15130758a79c1b4acd79014df
tobiitum <101997368+tobiitum@users.noreply.github.com>
2022-06-07 22:22:48 +0200

    Add files via upload

---
+++ src/com/tkdev/tumcalc/calc.java
@@ -0,0 +1,50 @@
+package com.tkdev.tumcalc;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class calc extends JFrame{
+
+    static JFrame frame;
+    private JPanel mainPanel;
+    private JButton aboutBut;
+    private JComboBox comboBox1;
+    private JButton setButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    String comboBoxListe[] = {"Informatik", "TUM-BWL","Aerospace"};
+
+    public static void main() {
+        frame = new calc("TUM Calculator");
+        frame.setSize(900, 500);
+        //icon = new ImageIcon("resources/netzwerk.png");
+        frame.setResizable(false);
+        frame.setMinimumSize(frame.getSize());
+        frame.setLocationRelativeTo(null);
+        //frame.setIconImage(icon.getImage());
+        frame.setVisible(true);
+    }
+
+    public calc(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+        aboutBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                About.main();
+            }
+        });
+    }
+
+
+
+}
---
+++ src/com/tkdev/tumcalc/main.java
@@ -0,0 +1,10 @@
+package com.tkdev.tumcalc;
+
+public class main {
+
+    public static void main(String[] args){
+            calc.main();
+    }
+
+
+}
