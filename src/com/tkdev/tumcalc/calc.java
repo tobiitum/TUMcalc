55dda4edf15a13a60e1cb31267619b7a5e06ccba
tobiitum <tbs.kling@gmail.com>
2022-07-05 22:16:20 +0200

    updated gui and internal use

--- src/com/tkdev/tumcalc/calc.java
+++ src/com/tkdev/tumcalc/calc.java
@@ -1,10 +1,12 @@
 package com.tkdev.tumcalc;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.net.URI;
 
 public class calc extends JFrame{
 
@@ -13,7 +15,7 @@ public class calc extends JFrame{
     private JButton aboutBut;
     private JComboBox comboBox1;
     private JButton setButton;
-    private JTextField textField1;
+    private JTextField resulttext;
     private JTextField mathe1;
     private JTextField mathe2;
     private JTextField mathe4;
@@ -74,6 +76,10 @@ public class calc extends JFrame{
     private JTextField kunst2;
     private JTextField kunst3;
     private JTextField kunst4;
+    private JButton helpButton;
+    private JLabel result;
+    private JComboBox comboBox2;
+    private JLabel abitext;
     String comboBoxListe[] = {"Informatik", "TUM-BWL","Aerospace"};
 
     public static void main() {
@@ -92,6 +98,7 @@ public class calc extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        setButton.doClick();
 
         aboutBut.addActionListener(new ActionListener() {
             @Override
@@ -283,19 +290,7 @@ public class calc extends JFrame{
             }
         });
 
-        abi.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                char c = e.getKeyChar();
-                if (((c < '0') || (c > '9')) && (c != KeyEvent.VK_BACK_SPACE)) {
-                    e.consume();
 
-                }
-                if (abi.getText().trim().length() == 2 || e.getKeyChar() == java.awt.event.KeyEvent.VK_BACK_SPACE) {
-                    e.consume();
-                }
-            }
-        });
         mathe1.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -389,7 +384,7 @@ public class calc extends JFrame{
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                abi.setText("");
+                abitext.setText("Punkte");
                 mathe1.setText("");
                 mathe2.setText("");
                 mathe3.setText("");
@@ -430,6 +425,178 @@ public class calc extends JFrame{
                 kunst3.setText("");
                 kunst4.setText("");
                 kunstfi.setText("");
+            }
+        });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setButton.doClick();
+            }
+        });
+        helpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Desktop desktop = java.awt.Desktop.getDesktop();
+                    URI oURL = new URI("https://tbkl.dev/index.php/projects/tumcalc");
+                    desktop.browse(oURL);
+                } catch (Exception f) {
+                    f.printStackTrace();
+                }
+            }
+        });
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!(abitext.getText() == "Punkte")){
+                    int abinote = Integer.parseInt(abitext.getText());
+                if (comboBox1.getSelectedItem() == "Aerospace"){
+                    int mathe0 = (Integer.parseInt(mathe1.getText() + mathe2.getText() + mathe3.getText() + mathe4.getText() + mathfi.getText())/5);
+                    int mathe = mathe0*3;
+                    int englisch = (Integer.parseInt(eng1.getText() + eng2.getText() + eng3.getText() + eng4.getText() + englischfi.getText())/5);
+                    if (!(infofi.getText() =="")){
+                        int informatik01 = (Integer.parseInt(info1.getText() + info2.getText() + info3.getText() + info4.getText() + infofi.getText())/5);
+                        int informatik02 = informatik01*2;
+                        int result = (abinote+(mathe+englisch+informatik02)/6)/2;
+                        resulttext.setText(String.valueOf(result));
+                    }
+                    else{ if (!(biofi.getText() =="")){
+                        int bio0 = (Integer.parseInt(bio1.getText() + bio2.getText() + bio3.getText() + bio4.getText() + biofi.getText())/5);
+                        int bio = bio0*2;
+                        int result = (abinote+(mathe+englisch+bio)/6)/2;
+                        resulttext.setText(String.valueOf(result));
+                    }else {
+                        if (!(chemiefi.getText() == "")) {
+                            int chemie0 = (Integer.parseInt(chemie1.getText() + chemie2.getText() + chemie3.getText() + chemie4.getText() + chemiefi.getText())/5);
+                            int chemie = chemie0 * 2;
+                            int result = (abinote+(mathe+englisch+chemie)/6)/2;
+                            resulttext.setText(String.valueOf(result));
+                        } else {
+                            if (!(physikfi.getText() == "")) {
+                                int physik0 = (Integer.parseInt(physik1.getText() + physik2.getText() + physik3.getText() + physik4.getText() + physikfi.getText())/5);
+                                int physik = physik0 * 2;
+                                int result = (abinote+(mathe+englisch+physik)/6)/2;
+                                resulttext.setText(String.valueOf(result));
+                            }
+                        }
+                    }}
+
+                }
+                if (comboBox1.getSelectedItem() == "Architektur") {
+                    int mathe0 = (Integer.parseInt(mathe1.getText() + mathe2.getText() + mathe3.getText() + mathe4.getText() + mathfi.getText())/5);
+                    int mathe = mathe0 * 2;
+                    int englisch = (Integer.parseInt(eng1.getText() + eng2.getText() + eng3.getText() + eng4.getText() + englischfi.getText())/5);
+                    int deutsch = (Integer.parseInt(deutsch1.getText() + deutsch2.getText() + deutsch3.getText() + deutschfi.getText() + deutsch4.getText())/5);
+                    int kunst0 = (Integer.parseInt(kunst1.getText() + kunst2.getText() + kunst3.getText() + kunst4.getText() + kunstfi.getText())/5);
+                    int kunst = kunst0*3;
+                    int result = (abinote+(mathe+englisch+deutsch+kunst)/6)/2;
+                    resulttext.setText(String.valueOf(result));
+                }
+                    if (comboBox1.getSelectedItem() == "Informatik") {
+                        int mathe0 = 100*(Integer.parseInt(mathe1.getText() + mathe2.getText() + mathe3.getText() + mathe4.getText() + mathfi.getText())/5);
+                        int mathe = mathe0 * 3;
+                        int englisch = (Integer.parseInt(eng1.getText() + eng2.getText() + eng3.getText() + eng4.getText() + englischfi.getText())/5);
+                        int deutsch = (Integer.parseInt(deutsch1.getText() + deutsch2.getText() + deutsch3.getText() + deutschfi.getText() + deutsch4.getText())/5);
+                        int informatik01 = (Integer.parseInt(info1.getText() + info2.getText() + info3.getText() + info4.getText() + infofi.getText())/5);
+                        int informatik02 = informatik01*2;
+                        int result = (abinote+(mathe+englisch+deutsch+informatik02)/7)/2;
+                        resulttext.setText(String.valueOf(mathe0));
+                    }
+                    if (comboBox1.getSelectedItem() == "TUM-BWL"){
+                        int mathe0 = Integer.parseInt(mathe1.getText() + mathe2.getText() + mathe3.getText() + mathe4.getText() + mathfi.getText());
+                        int mathe = mathe0*2;
+                        int englisch = Integer.parseInt(eng1.getText() + eng2.getText() + eng3.getText() + eng4.getText() + englischfi.getText());
+                        int deutsch = Integer.parseInt(deutsch1.getText() + deutsch2.getText() + deutsch3.getText() + deutschfi.getText() + deutsch4.getText());
+                        if (!(infofi.getText() =="")){
+                            int informatik01 = Integer.parseInt(info1.getText() + info2.getText() + info3.getText() + info4.getText() + infofi.getText());
+                            int informatik02 = informatik01*2;
+                            int result = (abinote+(mathe+englisch+deutsch+informatik02)/6)/2;
+                            resulttext.setText(String.valueOf(result));
+                        }
+                        else{ if (!(biofi.getText() =="")){
+                            int bio0 = Integer.parseInt(bio1.getText() + bio2.getText() + bio3.getText() + bio4.getText() + biofi.getText());
+                            int bio = bio0*2;
+                            int result = (abinote+(mathe+englisch+deutsch+bio)/6)/2;
+                            resulttext.setText(String.valueOf(result));
+                        }else {
+                            if (!(chemiefi.getText() == "")) {
+                                int chemie0 = Integer.parseInt(chemie1.getText() + chemie2.getText() + chemie3.getText() + chemie4.getText() + chemiefi.getText());
+                                int chemie = chemie0 * 2;
+                                int result = (abinote+(mathe+englisch+deutsch+chemie)/6)/2;
+                                resulttext.setText(String.valueOf(result));
+                            } else {
+                                if (!(physikfi.getText() == "")) {
+                                    int physik0 = Integer.parseInt(physik1.getText() + physik2.getText() + physik3.getText() + physik4.getText() + physikfi.getText());
+                                    int physik = physik0 * 2;
+                                    int result = (abinote+(mathe+englisch+deutsch+physik)/6)/2;
+                                    resulttext.setText(String.valueOf(result));
+                                }
+                            }
+                        }}
+
+                    }
+            }else{
+                    JOptionPane.showMessageDialog(frame,
+                            "Bitte lege die Abitur Note fest!",
+                            "Warning",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }});
+
+
+        comboBox2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (comboBox2.getSelectedItem() == "1,0" || comboBox2.getSelectedItem() == "1,1"){
+                    abitext.setText("15");
+                }
+                if (comboBox2.getSelectedItem() == "1,2" || comboBox2.getSelectedItem() == "1,3" || comboBox2.getSelectedItem() == "1,4" || comboBox2.getSelectedItem() == "1,5"){
+                    abitext.setText("14");
+                }
+                if (comboBox2.getSelectedItem() == "1,6" || comboBox2.getSelectedItem() == "1,7" || comboBox2.getSelectedItem() == "1,8"){
+                    abitext.setText("13");
+                }
+                if (comboBox2.getSelectedItem() == "1,9" || comboBox2.getSelectedItem() == "2,0" || comboBox2.getSelectedItem() == "2,1"){
+                    abitext.setText("12");
+                }
+                if (comboBox2.getSelectedItem() == "2,2" || comboBox2.getSelectedItem() == "2,3" || comboBox2.getSelectedItem() == "2,4" || comboBox2.getSelectedItem() == "2,5"){
+                    abitext.setText("11");
+                }
+                if (comboBox2.getSelectedItem() == "2,6" || comboBox2.getSelectedItem() == "2,7" || comboBox2.getSelectedItem() == "2,8"){
+                    abitext.setText("10");
+                }
+                if (comboBox2.getSelectedItem() == "2,9" || comboBox2.getSelectedItem() == "3,0" || comboBox2.getSelectedItem() == "3,1"){
+                    abitext.setText("9");
+                }
+                if (comboBox2.getSelectedItem() == "3,2" || comboBox2.getSelectedItem() == "3,3" || comboBox2.getSelectedItem() == "3,4" || comboBox2.getSelectedItem() == "3,5"){
+                    abitext.setText("8");
+                }
+                if (comboBox2.getSelectedItem() == "3,6" || comboBox2.getSelectedItem() == "3,7" || comboBox2.getSelectedItem() == "3,8") {
+                    abitext.setText("7");
+                }
+                if (comboBox2.getSelectedItem() == "3,9" || comboBox2.getSelectedItem() == "4,0" || comboBox2.getSelectedItem() == "4,1") {
+                    abitext.setText("6");
+                }
+                if (comboBox2.getSelectedItem() == "4,2" || comboBox2.getSelectedItem() == "4,3" || comboBox2.getSelectedItem() == "4,4" || comboBox2.getSelectedItem() == "4,5") {
+                    abitext.setText("5");
+                }
+                if (comboBox2.getSelectedItem() == "4,6" || comboBox2.getSelectedItem() == "4,7" || comboBox2.getSelectedItem() == "4,8") {
+                    abitext.setText("4");
+                }
+                if (comboBox2.getSelectedItem() == "4,9" || comboBox2.getSelectedItem() == "5,0" || comboBox2.getSelectedItem() == "5,1") {
+                    abitext.setText("3");
+                }
+                if (comboBox2.getSelectedItem() == "5,2" || comboBox2.getSelectedItem() == "5,3" || comboBox2.getSelectedItem() == "5,4" || comboBox2.getSelectedItem() == "5,5") {
+                    abitext.setText("2");
+                }
+                if (comboBox2.getSelectedItem() == "5,6" || comboBox2.getSelectedItem() == "5,7" || comboBox2.getSelectedItem() == "5,8") {
+                    abitext.setText("1");
+                }
+                if (comboBox2.getSelectedItem() == "5,9" || comboBox2.getSelectedItem() == "6,0") {
+                    abitext.setText("0");
+                }
+
+
             }
         });
     }
